22f5dc537f75cbedc785f0be51bc0936f4445ecf
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-01-03 14:31:20 -0500

    ac

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 console.log(document.cookie)
 
 console.log('v1.0.0')
-let jj = 'amu'
 setInterval(credits, 6000);
 
 function credits() {
     console.log('switching')
-    let doc = document.getElementById("eee").innerHTML
-    if(jj == 'park'){
-        jj = 'amu'
-        doc = 'Site programmed and designed by amukh1.'
-    }else if(jj == 'amu'){
-        jj = 'park'
-        doc = 'Parkour is a high speed intense game that requires a lot of energy and agility.'
+    if(document.getElementById('eee').innerHTML == 'Parkour is a high speed intense game that requires a lot of energy and agility.'){
+
+        document.getElementById('eee').innerHTML = 'Site programmed and designed by amukh1.'
+        //    document.getElementById('eee').innerHTML = 'amu'
+    }else if(document.getElementById('eee').innerHTML == 'Site programmed and designed by amukh1.'){
+
+        document.getElementById('eee').innerHTML = 'Parkour is a high speed intense game that requires a lot of energy and agility.'
+        
+        // document.getElementById('eee').innerHTML = 'park'
     }
 }
