220ce4877e84f592f3df6d6ff2ce19eee8348656
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-01-03 13:03:47 -0500

    aaa

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-console.log(document.cookie)
+setInterval(credits, 3000);
 
+function credits() {
+    console.log('switching')
+    document.getElementById("eee").innerHTML = 'Parkour is a high speed intense game that requires a lot of energy and agility.'
+}
