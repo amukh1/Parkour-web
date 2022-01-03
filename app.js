da3a578cdfb0c77bfab8cda5ed109a2c67ff37cd
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-01-03 13:22:01 -0500

    q

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 console.log(document.cookie)
 
 console.log('v1.0.0')
-
-setInterval(credits, 3000);
+let jj = 'amu'
+setInterval(credits, 6000);
 
 function credits() {
     console.log('switching')
     let doc = document.getElementById("eee").innerHTML
-    if(doc == 'Parkour is a high speed intense game that requires a lot of energy and agility.'){
+    if(jj == 'park'){
+        jj = 'amu'
         doc = 'Site programmed and designed by amukh1.'
-    }else if(doc == 'Site programmed and designed by amukh1.'){
+    }else if(jj == 'amu'){
+        jj = 'park'
         doc = 'Parkour is a high speed intense game that requires a lot of energy and agility.'
     }
 }
